78d937b34bfd947ef5a389b9be9d01e5b43d3c54
David Sanders <david.wm.sanders@gmail.com>
2020-07-10 05:39:29 +0100

    Add quickstart python script [closes #6]

---
+++ quickstart.py
@@ -0,0 +1,6 @@
+import webbrowser
+from urllib.parse import quote
+
+rwr_uri = "steam://rungameid/270150//"
+rwr_cmd = ["map=media/packages/_muttrah_map_project/maps/map18", "verbose", "debugmode", "metagame_debugmode", "no_ai"]
+webbrowser.open_new(f"{rwr_uri}{'%20'.join(quote(c, safe='') for c in rwr_cmd)}")
